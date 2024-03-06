0ef609b20ad4d23466960bd1a5c87690dd2f70df
d5_83636_Abhishek <abhishekballewar@gmail.com>
2024-03-06 23:44:45 +0530

    prep assign Ques10

---
+++ Ques10/src/Ques10.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Ques10 {
+
+	public static void main(String[] args) {
+		String[] arr = {"Hello","World","Java","Assignment","Ques10"};
+		Arrays.sort(arr);
+		int i;
+		for(i=0;i<arr.length;i++) {
+          System.out.println(arr[i]);
+	}
+
+}
+}
